5b2a787f56e8f3b0a8b71f3f08b7056dd07639a7
Antonioni Barros Campos <antonioni.campos@gmail.com>
2022-06-03 08:21:37 -0300

    add code to main block

--- main.py
+++ main.py
@@ -77,30 +77,31 @@ class EvolutionStrategy:
         print(f'\ngen {gen+1}, best fitnesses: {best_fitnesses[-5:]}')
 
 
-env = gym.make("LunarLander-v2")
+if __name__ == "__main__":
+    env = gym.make("LunarLander-v2")
 
-observation, info = env.reset(seed=42, return_info=True)
+    observation, info = env.reset(seed=42, return_info=True)
 
-input_dim = env.observation_space.shape[0]
-output_dim = env.action_space.n
-policy = NN(input_dim=input_dim, output_dim=output_dim, hidden_units=64)
+    input_dim = env.observation_space.shape[0]
+    output_dim = env.action_space.n
+    policy = NN(input_dim=input_dim, output_dim=output_dim, hidden_units=64)
 
-action = policy(observation)
-
-frames = []
-
-done = False
-total_reward = 0.0
-while not done:
-    env.render()
-    frames.append(env.render(mode='rgb_array'))
     action = policy(observation)
-    observation, reward, done, info = env.step(action)
-    total_reward += reward
 
-    if done:
-        print(total_reward)
-        observation, info = env.reset(return_info=True)
+    frames = []
 
-env.close()
-imageio.mimsave(os.path.join('.', 'movie.gif'), frames, format='GIF', fps=60)
+    done = False
+    total_reward = 0.0
+    while not done:
+        env.render()
+        frames.append(env.render(mode='rgb_array'))
+        action = policy(observation)
+        observation, reward, done, info = env.step(action)
+        total_reward += reward
+
+        if done:
+            print(total_reward)
+            observation, info = env.reset(return_info=True)
+
+    env.close()
+    imageio.mimsave(os.path.join('.', 'movie.gif'), frames, format='GIF', fps=60)
